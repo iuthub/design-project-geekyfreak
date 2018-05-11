a2c4482f07b3b1e7459a26f6a38ae7cc7de941aa
jasur4 <36454374+jasur4@users.noreply.github.com>
2018-05-11 22:28:59 +0500

    Update index.blade.php
    adding youtube api

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -5,8 +5,14 @@
                 <div class="card-header ">WELCOME to GEEKYFREAK</div>
                    </div>
               </div>
-                   
               <div class="d-flex flex-row flex-wrap ">
+
+                <iframe class="apivideo" src="//www.youtube.com/embed/?listType=user_uploads&list=ComicBookCom" width="660px" height="400px" frameborder="0"></iframe>
+                <div class="card flex-fill info" style="width:400px" >
+                                      <div class="card-body">
+                  Simple, convenient, innovative, creative… These are the main basics on which our site was created.
+Lots of people, especially young readers who are struggling to improve their reading skills, can benefit from the visual format of comic books and graphic novels. Several studies have indicated that comic books often appeal to readers who find other material too challenging or uninteresting. With their intense visuals and focus on plot and characters, comic books can be much more engaging than other literary media, such as novels or short stories.</div>
+</div>
                   @if(count($comics)>0) 
                     
                     @foreach($comics as $comic)
@@ -27,8 +33,6 @@
                     @endif
                     </div>
 
-                  
-
                     @if(count($comics)==0)
                     @if(count($mangas)==0)
                     @if(count($manhwas)==0)
@@ -36,8 +40,9 @@
                      @endif 
                      @endif
                      @endif
-                    
                      <br><br>
+
+
                     <div class="row justify-content-center">
                     
                   <div class="itempagination"></div>
